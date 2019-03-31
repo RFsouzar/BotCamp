cbe3d66c40a4d847ef3b8bc0202b701b0df45ba5
rafael <rafael.souza-silva@tecban.com.br>
2019-03-30 21:57:45 -0300

    Aula1 - Desafios: Todos Concluído

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
 import BtnBotcamp from './components/BtnBotcamp';
+import LogoBotcamp from './components/LogoBotcamp';
 
 
+const App = () => (
+    <section>
+        <LogoBotcamp />
+        <BtnBotcamp />
+        
+    </section>
+);
 
-class App extends Component{
+
+/*class App extends Component{
     render(){
-        return <BtnBotcamp />
+        return <section> <BtnBotcamp /> <LogoBotcamp /> </section>
+            
     }
-}
+}*/
 
 export default App;
---
+++ app/src/components/LogoBotcamp/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BotCampImg from "../../img/botcamp.png";
+import '../LogoBotcamp/style.css';
+
+const LogoBotcamp = () => (
+    <img className="log-botcamp" src={BotCampImg} alt="botcamp"></img>
+);
+
+export default LogoBotcamp;
+
